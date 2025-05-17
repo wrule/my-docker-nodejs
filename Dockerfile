c326df9b25a789254822b3c92848d37fb157b9a6
jimao <shihao.gu@perfma.com>
2025-05-17 15:24:19 +0800

    feat: build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=linux/amd64 node:22.15.1
+FROM node:22.15.1
 
 RUN apt update && apt install -y \
   vim \
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --platform=amd64 -t my-image .
+docker buildx build --platform=amd64 -t my-image .
